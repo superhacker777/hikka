d5dc19236118b60779f047dadb50027ff212b96d
shenjl <shenjl@qq.com>
2021-12-03 16:01:33 +0800

    fix6

--- bin/hikka.sh
+++ bin/hikka.sh
@@ -4,7 +4,7 @@ nohup ./hikka > hikka.out 2>&1 &
 ps -ef | grep hikka 
 
 
-cd rtsp-server
-ps -ef |grep rtsp-server-linux | grep -v grep | awk '{print $2}' | xargs -r kill -9
-nohup ./hikka > hikka.out 2>&1 &
-ps -ef | grep hikka 
+# cd rtsp-server
+# ps -ef |grep rtsp-server-linux | grep -v grep | awk '{print $2}' | xargs -r kill -9
+# nohup ./hikka > hikka.out 2>&1 &
+# ps -ef | grep hikka 
